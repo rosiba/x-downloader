51d51ba6cdbed70fffc03ef16480fda420823a59
Rojhat Sinan Balka <9906640+rosiba@users.noreply.github.com>
2026-02-20 20:36:27 +0300

    Add bot and webhook setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module x-downloader
 
 go 1.26
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	botapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	key := os.Getenv("TELEGRAM_BOT_API_KEY")
+	if key == "" {
+		log.Panic("TELEGRAM_BOT_API_KEY environment variable not set")
+	}
+
+	webHookURL := os.Getenv("TELEGRAM_WEBHOOK_URL")
+	if webHookURL == "" {
+		log.Panic("TELEGRAM_WEBHOOK_URL environment variable not set")
+	}
+
+	bot, err := botapi.NewBotAPI(key)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	wh, err := botapi.NewWebhook(webHookURL + "/" + bot.Token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if _, err := bot.Request(wh); err != nil {
+		log.Panic(err)
+	}
+
+	log.Println("Bot is now running.")
 }
